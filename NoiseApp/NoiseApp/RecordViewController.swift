0585af9e8bbf56e27a08dd5df9440f9602182c8a
jacobblair <jsb0087@auburn.edu>
2022-09-14 02:51:55 -0500

    Added protection warning and removed background animations

--- NoiseApp/NoiseApp/RecordViewController.swift
+++ NoiseApp/NoiseApp/RecordViewController.swift
@@ -15,6 +15,7 @@ class RecordViewController: UIViewController, AVAudioRecorderDelegate
 {
     // UI elements
     @IBOutlet weak var decibel: UILabel!
+    @IBOutlet weak var ProtectionRecommendation: UILabel!
     @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var stopRecordingButton: UIButton!
@@ -73,25 +74,26 @@ class RecordViewController: UIViewController, AVAudioRecorderDelegate
         let dBFS = audioRecorder.averagePower(forChannel: 0)
         let dB = dBFS + 100
         decibel.text = String(format: "%.0f dB", dB)
-        if (dB < 80) {
+        if (dB < 65) {
             UIView.animate(withDuration: 1) {
-                self.statusLabel.textColor = UIColor.init(displayP3Red: 1.0, green: 0.435, blue: 0.32, alpha: 1)
-                self.stopRecordingButton.tintColor = UIColor.init(displayP3Red: 1.0, green: 0.435, blue: 0.32, alpha: 1)
-                self.view.backgroundColor = UIColor.init(displayP3Red: 0.0, green: 0.4, blue: 0.0, alpha: 1)
+//                self.statusLabel.textColor = UIColor.init(displayP3Red: 1.0, green: 0.435, blue: 0.32, alpha: 1)
+//                self.stopRecordingButton.tintColor = UIColor.init(displayP3Red: 1.0, green: 0.435, blue: 0.32, alpha: 1)
+//                self.view.backgroundColor = UIColor.init(displayP3Red: 0.0, green: 0.4, blue: 0.0, alpha: 1)
             }
         }
-        else if (80 < dB && dB < 95) {
+        else if (65 < dB && dB < 85) {
             UIView.animate(withDuration: 1) {
-                self.statusLabel.textColor = UIColor.init(displayP3Red: 0.01, green: 0.14, blue: 0.30, alpha: 1)
-                self.stopRecordingButton.tintColor = UIColor.init(displayP3Red: 0.01, green: 0.14, blue: 0.30, alpha: 1)
-                self.view.backgroundColor = UIColor.init(displayP3Red: 0.93, green: 0.82, blue: 0.01, alpha: 1)
+//                self.statusLabel.textColor = UIColor.init(displayP3Red: 0.01, green: 0.14, blue: 0.30, alpha: 1)
+//                self.stopRecordingButton.tintColor = UIColor.init(displayP3Red: 0.01, green: 0.14, blue: 0.30, alpha: 1)
+//                self.view.backgroundColor = UIColor.init(displayP3Red: 0.93, green: 0.82, blue: 0.01, alpha: 1)
             }
         }
-        else if (dB > 95) {
+        else if (dB > 85) {
             UIView.animate(withDuration: 1) {
-                self.statusLabel.textColor = UIColor.init(displayP3Red: 0.00, green: 0.75, blue: 0.79, alpha: 1)
-                self.stopRecordingButton.tintColor = UIColor.init(displayP3Red: 0.00, green: 0.75, blue: 0.79, alpha: 1)
-                self.view.backgroundColor = UIColor.init(displayP3Red: 0.79, green: 0.04, blue: 0.0, alpha: 1)
+//                self.statusLabel.textColor = UIColor.init(displayP3Red: 0.00, green: 0.75, blue: 0.79, alpha: 1)
+//                self.stopRecordingButton.tintColor = UIColor.init(displayP3Red: 0.00, green: 0.75, blue: 0.79, alpha: 1)
+//                self.view.backgroundColor = UIColor.init(displayP3Red: 0.79, green: 0.04, blue: 0.0, alpha: 1)
+                self.ProtectionRecommendation.textColor = UIColor.init(displayP3Red: 0.392, green: 0.11, blue: 0, alpha: 1)
             }
             
         }
